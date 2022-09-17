591f58da119e7d1eb81c14babb76d3afdf8b6c5c
yaroslav_admin <I-love-linux-12-31@users.noreply.github.com>
2022-09-17 23:09:20 +0300

    Lesson 5  - task 4.25 : added

--- Lesson5/Rows/35.cpp
+++ Lesson5/Rows/35.cpp
@@ -1,3 +1,29 @@
-//
-// Created by yaroslav_admin on 14.09.22.
-//
+// Lesson 5
+
+#include <iostream>
+
+
+int main () {
+    std::cout << "Введите строку :" << std::endl;
+    std::string line;
+
+    std::getline(std::cin, line);
+    int count = 0;
+    int max_spaces = 0;
+    for (int i = 0; i < line.length(); i++){
+        if (line[i] == ' '){
+            count++;
+        }
+        else {
+            if (count > max_spaces){
+                max_spaces = count;
+            }
+            count = 0;
+        }
+    }
+    if (count >> 0 and max_spaces < count){
+        max_spaces = count;
+    }
+    std::cout << "Максимальное число подряд идущих пробелов :" << max_spaces << std::endl;
+    return 0;
+}
