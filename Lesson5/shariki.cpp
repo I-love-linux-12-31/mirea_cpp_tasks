f53c80f9b47d61e3337253b3881b3f641b09ba67
yaroslav_admin <I-love-linux-12-31@users.noreply.github.com>
2022-11-12 23:45:57 +0300

    Additional tasks:  shariki.cpp : done

--- Lesson5/shariki.cpp
+++ Lesson5/shariki.cpp
@@ -1,16 +1,98 @@
 // Lesson 5
 #include <iostream>
+#include <vector>
+#include "../cli_data_reader.h++"
 
-int factorial (int a) {
-    int result = 1;
-    for (int i = 1; i <= a; i++) {
-        result *= i;
+std::vector<std::vector<int>> get_variants(std::vector<std::vector<int>> data, std::vector<int> variants, int level){
+//    std::cout << "Called with " << data.size() << " " << variants.size() << " Recursion :" << level << std::endl;
+//    for (auto line : data){
+//        for (int i : line){
+//            std::cout << i << " ";
+//        }
+//        std::cout << std::endl;
+//    }
+//    if (data.empty())
+//        std::cout << "EMPTY" << std::endl;
+//    std::cout << "---------------" << std::endl;
+//    for (int i : variants){
+//        std::cout << i << " ";
+//    }
+//    std::cout << std::endl;
+//    std::cout << "===============" << std::endl;
+
+    std::vector<std::vector<int>> result;
+    std::vector<int> variants_temp;
+    if (variants.empty())
+        return data;
+
+    for (int v_index = 0 ; v_index < variants.size(); v_index++){
+        variants_temp = variants;
+        int current_vr = variants_temp[v_index];
+        variants_temp.erase(variants_temp.begin() + v_index);
+        std::vector<std::vector<int>> data_temp;
+        //for (int vr : variants_temp){
+            data_temp = data;
+//            for (auto line : data_temp){ // WTF Error?!
+//                line.push_back(current_vr);
+//            }
+            for (int line_id = 0; line_id < data_temp.size(); line_id++){
+                data_temp[line_id].push_back(current_vr);
+            }
+            if (data.empty()){
+                data_temp.push_back({current_vr});
+            }
+
+
+            for (auto line : get_variants(data_temp, variants_temp, level + 1)){
+
+                result.push_back(line);
+            }
+
+        //}
     }
     return result;
 }
 
+
+bool is_correct(std::vector<int> _line){
+    for (int i = 0; i < _line.size(); i++){
+        if (i == _line[i])
+            return true;
+    }
+    return false;
+
+}
+
+
+int get_target_combinations_count ( std::vector<std::vector<int>> _data){
+    int count = 0;
+    for (auto _line : _data){
+        if (is_correct(_line))
+            count++;
+    }
+    return count;
+}
+
+
+
 int main () {
-    int n = 10;
-    std::cout << "Нашлось " << factorial(n - 1)*n << " варианта(ов)" << std::endl;
+    std::cout << "Сколько шариков?" << std::endl;
+    int n = get_int_more_0_from_user();
+//    std::cout << "Нашлось " << factorial(n - 1)*n << " варианта(ов)" << std::endl;
+    std::cout << "Start" << std::endl;
+    std::vector<int> numbers;
+    for (int j = 0; j < n; j++){
+        numbers.push_back(j);
+    }
+
+    auto res = get_variants({}, numbers, 0);
+//    for (auto line : res){
+//        std::cout << "#";
+//        for (int i : line){
+//            std::cout << i << " ";
+//        }
+//        std::cout << std::endl;
+//    }
+    std::cout << "Нашлось " << get_target_combinations_count(res) << " варианта(ов) из " << res.size() << std::endl;
 
 }
