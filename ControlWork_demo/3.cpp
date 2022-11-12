36e0012ee3f0f3b0ec0caf7c667c1a472173181f
yaroslav_admin <I-love-linux-12-31@users.noreply.github.com>
2022-11-13 00:51:32 +0300

    Control Work Demo:  3: added

---
+++ ControlWork_demo/3.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yaroslav_admin on 13.11.22.
+//
+#include <iostream>
+
+int main(){
+    int a, b;
+    std::cout << a * (a + 1) * b * (b + 1) / 4 << std::endl;
+    // По этой формуле вычисляется сколько прямоугольников можно получить из сетки AxB
+}
