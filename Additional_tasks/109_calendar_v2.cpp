ebbd7be6b69e39a483945e542ff1fd7a98ec2ab9
I-love-linux-12-31@users.noreply.github.com <I-love-linux-12-31@users.noreply.github.com>
2022-12-13 18:32:56 +0300

    109 in progress

--- Additional_tasks/109_calendar_v2.cpp
+++ Additional_tasks/109_calendar_v2.cpp
@@ -4,12 +4,25 @@
 
 #include <iostream>
 
+int get_any_int_from_user() {
+    int result;
+    std::string buffer;
+    getline(std::cin, buffer);
+    while (sscanf(buffer.c_str(), "%d", &result) != 1) {
+        std::cout << "Не корректный ввод! Должно быть целое число, например 10 !" << std::endl;
+        std::cout << "Введите число :";
+
+        getline(std::cin, buffer);
+    }
+    return result;
+}
+
 int main(){
     int in_days, in_months, in_years;
     int now_days, now_months, now_years;
-    in_days = 16;
-    in_months = 3;
-    in_years = 2004;
+    in_days = get_any_int_from_user();
+    in_months = get_any_int_from_user();
+    in_years = get_any_int_from_user();
     now_days = 13;
     now_months = 12;
     now_years = 2022;
@@ -18,7 +31,7 @@ int main(){
     int dd = 0, dm = 0, dy = 0;
     dd = now_days - in_days;
     if (dd < 0){
-        dd += 31;
+        dd += 30;
         dm -= 1;
     }
     dm += now_months - in_months;
